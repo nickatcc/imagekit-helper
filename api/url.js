c2db5969293b8544763cb41d5b82421dd5f08aab
nickatcc <nick@combinedcuriosity.com>
2025-08-19 10:16:50 -0400

    Update url.js

--- api/url.js
+++ api/url.js
@@ -7,9 +7,12 @@ const ik = new ImageKit({
 });
 
 export default async function handler(req, res) {
-  const { path, layers = [] } = req.body || {};
+  if (req.method === "GET") {
+    return res.status(200).json({ ok: true, message: "Helper is alive!" });
+  }
 
-  const transformation = layers.map(raw => ({ raw }));
+  const { path, layers = [] } = req.body || {};
+  const transformation = layers.map((raw) => ({ raw }));
 
   const url = ik.url({
     path,
